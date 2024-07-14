bc2c2e1fc69742454ed09cef47deb05a1320292a
Ashutosh Pratap Singh <112776372+ashk6645@users.noreply.github.com>
2024-07-14 09:36:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Reverse digits/reverse-digits.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            long n = Long.parseLong(br.readLine().trim());            
+            Solution ob = new Solution();
+            long ans = ob.reverse_digit(n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public long reverse_digit(long n)
+    {
+       long rev=0, digit;
+        while(n!=0)
+        {
+            digit=n%10;
+            rev=rev*10+digit;
+            n=n/10;
+            
+            if(rev==0)
+            {
+                continue;
+            }
+        }
+        return rev;
+    }
+}
