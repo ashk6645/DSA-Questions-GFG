128f906ec3c1f440063e9665e01c90ac9869bc30
Ashutosh Pratap Singh <112776372+ashk6645@users.noreply.github.com>
2024-08-22 09:08:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.java
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+
+public class GFG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+        while (t-- > 0) {
+            List<Integer> arr = new ArrayList<>();
+            List<Integer> brr = new ArrayList<>();
+            List<Integer> crr = new ArrayList<>();
+
+            String input1 = sc.nextLine();
+            Scanner ss1 = new Scanner(input1);
+            while (ss1.hasNextInt()) {
+                arr.add(ss1.nextInt());
+            }
+
+            String input2 = sc.nextLine();
+            Scanner ss2 = new Scanner(input2);
+            while (ss2.hasNextInt()) {
+                brr.add(ss2.nextInt());
+            }
+
+            String input3 = sc.nextLine();
+            Scanner ss3 = new Scanner(input3);
+            while (ss3.hasNextInt()) {
+                crr.add(ss3.nextInt());
+            }
+
+            Solution ob = new Solution();
+            List<Integer> res = ob.commonElements(arr, brr, crr);
+
+            if (res.size() == 0) System.out.print(-1);
+            for (int i = 0; i < res.size(); i++) System.out.print(res.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to find common elements in three arrays.
+    public List<Integer> commonElements(List<Integer> arr1, List<Integer> arr2,
+                                        List<Integer> arr3) 
+   {
+                                            
+        // Code Here
+         Set<Integer> set1 = new LinkedHashSet<>(arr1);
+        Set<Integer> set2 = new LinkedHashSet<>();
+        Set<Integer> set3 = new LinkedHashSet<>();
+
+        for (int num : arr2) {
+            if (set1.contains(num)) {
+                set2.add(num);
+            }
+        }
+
+        for (int num : arr3) {
+            if (set2.contains(num)) {
+                set3.add(num);
+            }
+        }
+
+        // Return the common elements as a list.
+        return new ArrayList<>(set3);
+    }
+}
