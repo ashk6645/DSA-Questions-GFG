8915bc8cbdfffb59a0d366e187ffefd8f6057d45
Ashutosh Pratap Singh <112776372+ashk6645@users.noreply.github.com>
2024-09-03 16:19:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum number of deletions and insertions/minimum-number-of-deletions-and-insertions.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String s1 = sc.next();
+                    String s2 = sc.next();
+                    Solution ob = new Solution();
+                    System.out.println(ob.minOperations(s1,s2));
+                }
+        }
+}    
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+	public int minOperations(String str1, String str2) 
+	{ 
+	    // Your code goes here
+	    int n = str1.length();
+	    int m = str2.length();
+	    int[][] arr = new int[n+1][m+1];
+	    for(int i=1;i<=n;i++)
+	    {
+	        for(int j=1;j<=m;j++)
+	        {
+	            if(str1.charAt(i-1)==str2.charAt(j-1))
+	            {
+	                arr[i][j] = 1+arr[i-1][j-1];
+	            }
+	            else
+	            {
+	                arr[i][j] = Math.max(arr[i-1][j],arr[i][j-1]);
+	            }
+	        }
+	    }
+	    return ((str1.length()+str2.length())-2*arr[n][m]);
+	    
+	} 
+}
