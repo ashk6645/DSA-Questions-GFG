fc24cc4e580ee288bf7e977ed02f074999e53305
Ashutosh Pratap Singh <112776372+ashk6645@users.noreply.github.com>
2024-12-24 15:22:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Search in a sorted Matrix/search-in-a-sorted-matrix.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t > 0) {
+            int rows = sc.nextInt();
+            int columns = sc.nextInt();
+
+            int matrix[][] = new int[rows][columns];
+
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    matrix[i][j] = sc.nextInt();
+                }
+            }
+            int target = sc.nextInt();
+
+            Solution x = new Solution();
+
+            if (x.searchMatrix(matrix, target))
+                System.out.println("true");
+            else
+                System.out.println("false");
+            t--;
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+
+class Solution {
+    // Function to search a given number in row-column sorted matrix.
+    public boolean searchMatrix(int[][] mat, int x) {
+        // code here
+        int n = mat.length;
+        int m = mat[0].length;
+        int left = 0;
+        int right = n*m-1;
+        while(left <= right){
+            int mid =left + (right - left)/2;
+            int midValue = mat[mid/m][mid%m];
+            if(midValue == x){
+                return true;
+            }
+            else if(midValue < x){
+                left = mid+1;
+            }
+            else{
+                right = mid-1;
+            }
+        }
+        return false;
+    }
+}
